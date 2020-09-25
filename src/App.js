a8bcdaaf63d153dd9913b6e12d1cb8b380a271df
ZakirBangash <engrzakirbangash@gmail.com>
2020-09-26 00:22:56 +0500

    Modal
    Modal is done yet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Posts from "./Posts";
 import { db } from './firebase'
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import Button from '@material-ui/core/Button'
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+
 
 
 function App() {
@@ -27,33 +48,82 @@ function App() {
   ]);
 
 
-useEffect(() => {
- 
-  
-  db.collection('posts').onSnapshot(snapshot=>{
-   setPost(snapshot.docs.map(doc => doc.data()))
-    
-  })
+  useEffect(() => {
 
-}, [])
+
+    db.collection('posts').onSnapshot(snapshot => {
+      setPost(snapshot.docs.map(doc => doc.data()))
+
+    })
+
+  }, [])
+
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
 
   return (
     <div className="App">
       <div className="app__header">
-        <img
+        <img className="app__image"
           src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
           alt=""
         />
+        <Button className='modal__button' type="button" onClick={handleOpen}>
+          SignUp
+      </Button>
       </div>
 
       <h1>
         Hello Clever Programmers , i React developer doing instagram clone
       </h1>
-      {post.map(({username,imageUrl,caption}) => {
+      {post.map(({ username, imageUrl, caption }) => {
+
+        return (
+          <Posts imageUrl={imageUrl} UserName={username} Caption={caption} />
+        )
+      })}
+
+
+
+
+
+  
         
-        return(
-        <Posts imageUrl={imageUrl} UserName={username} Caption={caption} />
-      )})}
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          className={classes.modal}
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={open}>
+            <div className={classes.paper}>
+              <h2 id="transition-modal-title">Transition modal</h2>
+              <p id="transition-modal-description">react-transition-group animates me.</p>
+            </div>
+          </Fade>
+        </Modal>
+  
+
+
+
+
+
     </div>
   );
 }
