65f0cccc81fc305a699afc91b8d8d335ce062b5b
ZakirBangash <engrzakirbangash@gmail.com>
2020-09-26 15:23:49 +0500

    half of the authentication
    Done with half of the authentication

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import Posts from "./Posts";
-import { db } from './firebase'
+import { db,auth } from './firebase'
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
 import Backdrop from '@material-ui/core/Backdrop';
 import Fade from '@material-ui/core/Fade';
 import Button from '@material-ui/core/Button'
+import { Input } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   modal: {
@@ -19,6 +20,7 @@ const useStyles = makeStyles((theme) => ({
     border: '2px solid #000',
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
+    width:500,
   },
 }));
 
@@ -26,40 +28,45 @@ const useStyles = makeStyles((theme) => ({
 
 
 function App() {
-  const [post, setPost] = useState([
-    // {
-    //   imageUrl:
-    //     "https://images.unsplash.com/photo-1600194991617-317f25e77508?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop",
-    //   UserName: "Zakir Bangash",
-    //   Caption: "My first Post on instagram clone",
-    // },
-    // {
-    //   imageUrl:
-    //     "https://images.unsplash.com/photo-1599722758280-6a90187b521c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&auto=format&fit=crop",
-    //   UserName: "Malik ShahZain",
-    //   Caption: "I just joined Instagram really fun",
-    // },
-    // {
-    //   imageUrl:
-    //     "https://images.unsplash.com/photo-1599751449029-e4ea8f21d966?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjczMTc0fQ&auto=format&fit=crop",
-    //   UserName: "Ayesha",
-    //   Caption: "I love the Developer of this clone",
-    // },
-  ]);
-
+  const [post, setPost] = useState([]);
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
 
-
     db.collection('posts').onSnapshot(snapshot => {
       setPost(snapshot.docs.map(doc => doc.data()))
-
     })
 
+  }, []);
+
+  useEffect(() => {
+    
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // User is signed in.
+        console.log(authUser);
+        setUser(authUser);      
+
+        // ...
+      } else {
+        // User is signed out.
+        // ...
+      }
+    });
+
+
+    // return () => {
+    //   cleanup
+    // }
   }, [])
+
 
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
+
 
   const handleOpen = () => {
     setOpen(true);
@@ -69,6 +76,24 @@ function App() {
     setOpen(false);
   };
 
+  const signUp = (event) => {
+    event.preventDefault();
+    
+    auth.createUserWithEmailAndPassword(email, password).then((result)=>{
+      return result.user.updateProfile({
+        displayName:username
+      })
+      
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      
+      var errorMessage = error.message;
+      alert(errorCode,errorMessage)
+      // ...
+    });
+
+  }
 
 
   return (
@@ -97,28 +122,41 @@ function App() {
 
 
 
-  
-        
-        <Modal
-          aria-labelledby="transition-modal-title"
-          aria-describedby="transition-modal-description"
-          className={classes.modal}
-          open={open}
-          onClose={handleClose}
-          closeAfterTransition
-          BackdropComponent={Backdrop}
-          BackdropProps={{
-            timeout: 500,
-          }}
-        >
-          <Fade in={open}>
-            <div className={classes.paper}>
-              <h2 id="transition-modal-title">Transition modal</h2>
-              <p id="transition-modal-description">react-transition-group animates me.</p>
-            </div>
-          </Fade>
-        </Modal>
-  
+
+
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <center>
+            <img 
+              src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+              alt=""
+            />
+            </center>
+
+            <form className="app__modal__form">
+
+            <Input className="input" value={username} onChange={(e)=> setUsername(e.target.value)}  type="text" placeholder="Enter Username"  fullWidth={true} required={true}	 />
+            <Input className="input" value={email} onChange={(e)=> setEmail(e.target.value)} type="email" placeholder="Enter Email"  fullWidth={true}/>
+            <Input className="input" value={password} onChange={(e)=> setPassword(e.target.value)} type="password" placeholder="Enter Password" fullWidth={true} />
+            <Button className="input sign" onClick={signUp} variant="contained" color="primary" fullWidth={true}>SignUp</Button>
+            
+            </form>
+          </div>
+        </Fade>
+      </Modal>
+
 
 
 
